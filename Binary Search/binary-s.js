4d184db55a84f0800d5df6c96cafc6b9f4f4b340
Debug-001 <ombhatt1158@gmail.com>
2024-07-16 16:45:56 +0530

    binary search order

---
+++ Binary Search/binary-s.js
@@ -0,0 +1,34 @@
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    //this may exceed interger value for int range
+    //  let mid = Math.floor((start + end)/ 2);
+    // this below one won't
+    // note that mid is here the index not element.
+    let mid = Math.floor(start + (end - start) / 2);
+
+    // arr[mid]returns the element in the array at mid index then its compares to target. if target less than mid index element.
+    if(target < arr[mid]){
+        // we change end to mid index - 1
+        end = mid - 1
+    }
+
+    else if (target > arr[mid]){
+        // if target > mid index element
+        // start becomes mid index + 1
+        start = mid + 1
+        // and we again return mid
+    }
+    else {
+        return mid;
+    }
+}
+return -1
+}
+
+let arr = [90,75,18,12,6,4,3,1]
+let target = 90
+let res = binarySearch (arr,target)
+console.log(res);
